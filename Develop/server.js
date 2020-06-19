7d961a0c7356b9a74e4641c114eab1e486f06adf
Peter Sammon <petersammon2@gmail.com>
2020-06-19 16:59:21 -0400

    set up .env

--- Develop/server.js
+++ Develop/server.js
@@ -6,12 +6,14 @@ require('dotenv').config();
 
 // Use mysql to connect to your mysql database and perform queries.
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'F15h3567!',
-    database: 'employees_db'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
+
+
 
 connection.connect((err) => {
     if (err)
